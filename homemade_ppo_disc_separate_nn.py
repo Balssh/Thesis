78c1d3c2dd6dc1c8d7a4cdb79e9518fad3bdcabd
Balssh <george.bals25@gmail.com>
2022-12-04 23:38:59 +0200

    closer version to original by results

--- homemade_ppo_disc_separate_nn.py
+++ homemade_ppo_disc_separate_nn.py
@@ -27,6 +27,7 @@ HYPER_PARAMS = {
     "UPDATE_EPOCHS": 4,
     "NORMALIZE_ADVANTAGE": True,
     "CLIP_VALUELOSS": True,
+    "NORMALIZE_GRADIENTS": True,
     "CLIPPING_COEFFICIENT": 0.2,
     "ENTROPY_COEFFICIENT": 0.01,
     "VALUE_LOSS_COEFFICIENT": 0.5,
@@ -134,15 +135,12 @@ if __name__ == "__main__":
     assert isinstance(
         envs.single_action_space, gym.spaces.Discrete
     ), "Only discrete action spaces supported!"
-    print("SUCCESSFULLY CREATED ENVIRONMENTS")
 
     # Create policy
     policy = Policy(envs).to(device)
     optimizer = optim.Adam(
         policy.parameters(), lr=HYPER_PARAMS["LEARNING_RATE"], eps=1e-5
     )
-    print(policy)
-    print("SUCCESSFULLY CREATED POLICY")
 
     # Create buffers
     obs = torch.zeros(
@@ -170,7 +168,7 @@ if __name__ == "__main__":
     start_time = time.time()
     next_obs = torch.Tensor(envs.reset()[0]).to(device)
     next_terminal = torch.zeros(HYPER_PARAMS["ENV_NUM"]).to(device)
-    num_updates = HYPER_PARAMS["TOTAL_TIMESTEPS"] // HYPER_PARAMS["BATCH_SIZE"]
+    num_updates = HYPER_PARAMS["TIMESTEPS"] // HYPER_PARAMS["BATCH_SIZE"]
 
     # Begin the training loop
     for update in range(1, num_updates + 1):
@@ -186,18 +184,17 @@ if __name__ == "__main__":
             obs[step] = next_obs
             terminateds[step] = next_terminal
             with torch.no_grad():
-                actions[step], log_probs[step], _, values = policy.get_action_and_value(
-                    obs[step]
-                )
-                values[step] = values.flatten()
-            # Step environments
-            next_obs, rewards[step], next_terminal, _, info = envs.step(
-                actions[step].cpu().numpy()
-            )
+                action, log_prob, _, value = policy.get_action_and_value(next_obs)
+                values[step] = value.flatten()
+
+            actions[step] = action
+            log_probs[step] = log_prob
+
+            next_obs, reward, next_terminal, _, info = envs.step(action.cpu().numpy())
+            rewards[step] = torch.tensor(reward).to(device).view(-1)
             next_obs = torch.Tensor(next_obs).to(device)
             next_terminal = torch.Tensor(next_terminal).to(device)
-
-            # Log rewards
+            # Log episode returns and lengths
             if "final_info" in info.keys():
                 for j, r in enumerate(info["final_info"]):
                     if r is not None:
@@ -207,3 +204,187 @@ if __name__ == "__main__":
                         writer.add_scalar(
                             "charts/episodic_length", r["episode"]["l"], global_step
                         )
+
+        # Compute advantages
+        with torch.no_grad():
+            next_value = policy.get_value(next_obs).reshape(1, -1)
+            if HYPER_PARAMS["USE_GAE"]:
+                # Generalized Advantage Estimation
+                advantages = torch.zeros_like(rewards).to(device)
+                last_advantage = 0
+                for t in reversed(range(HYPER_PARAMS["ENV_TIMESTEPS"])):
+                    if t == HYPER_PARAMS["ENV_TIMESTEPS"] - 1:
+                        next_nonterminal = 1.0 - next_terminal
+                        next_return = next_value
+                    else:
+                        next_nonterminal = 1.0 - terminateds[t + 1]
+                        next_return = values[t + 1]
+                    # Calculate delta
+                    delta = (
+                        rewards[t]
+                        + HYPER_PARAMS["GAE_GAMMA"] * next_nonterminal * next_return
+                        - values[t]
+                    )
+                    # Calculate advantage
+                    advantages[t] = last_advantage = (
+                        delta
+                        + HYPER_PARAMS["GAE_GAMMA"]
+                        * HYPER_PARAMS["GAE_LAMBDA"]
+                        * next_nonterminal
+                        * last_advantage
+                    )
+                returns = advantages + values
+            else:
+                # Calculate returns using discounted rewards
+                returns = torch.zeros_like(rewards).to(device)
+                for t in reversed(range(HYPER_PARAMS["ENV_TIMESTEPS"])):
+                    if t == HYPER_PARAMS["ENV_TIMESTEPS"] - 1:
+                        next_nonterminal = 1.0 - next_terminal
+                        next_return = next_value
+                    else:
+                        next_nonterminal = 1.0 - terminateds[t + 1]
+                        next_return = returns[t + 1]
+                    returns[t] = (
+                        rewards[t]
+                        + HYPER_PARAMS["GAE_GAMMA"] * next_nonterminal * next_return
+                    )
+                advantages = returns - values
+
+        # Batch data
+        batch_obs = obs.reshape((-1,) + envs.single_observation_space.shape)
+        batch_actions = actions.reshape((-1,) + envs.single_action_space.shape)
+        batch_log_probs = log_probs.reshape(-1)
+        batch_returns = returns.reshape(-1)
+        batch_values = values.reshape(-1)
+        batch_advantages = advantages.reshape(-1)
+
+        # Optimize policy
+        batch_ind = np.arange(HYPER_PARAMS["BATCH_SIZE"])
+        clipped_fracs = []
+        for epoch in range(HYPER_PARAMS["UPDATE_EPOCHS"]):
+            # Shuffle batch
+            np.random.shuffle(batch_ind)
+
+            # Iterate over batches
+            for start in range(
+                0, HYPER_PARAMS["BATCH_SIZE"], HYPER_PARAMS["MINIBATCH_SIZE"]
+            ):
+                # Get minibatch indices
+                end = start + HYPER_PARAMS["MINIBATCH_SIZE"]
+                minibatch_ind = batch_ind[start:end]
+
+                # Get values and log probs
+                _, new_log_probs, entropy, new_value = policy.get_action_and_value(
+                    batch_obs[minibatch_ind], batch_actions.long()[minibatch_ind]
+                )
+
+                # Calculate ratios between old and new policy
+                log_ratio = new_log_probs - batch_log_probs[minibatch_ind]
+                ratio = torch.exp(log_ratio)
+
+                # Calculate KL divergence
+                with torch.no_grad():
+                    old_kl = (-log_probs).mean()
+                    new_kl = ((ratio - 1) - log_ratio).mean()
+                    clipped_fracs += [
+                        ((ratio - 1).abs() > HYPER_PARAMS["CLIPPING_COEFFICIENT"])
+                        .float()
+                        .mean()
+                        .item()
+                    ]
+
+                # Normalize advantages
+                minibatch_advantages = batch_advantages[minibatch_ind]
+                if HYPER_PARAMS["NORMALIZE_ADVANTAGE"]:
+                    minibatch_advantages = (
+                        minibatch_advantages - minibatch_advantages.mean()
+                    ) / (minibatch_advantages.std() + 1e-8)
+
+                # Calculate surrogate loss
+                policy_loss1 = -ratio * minibatch_advantages
+                policy_loss2 = -minibatch_advantages * torch.clamp(
+                    ratio,
+                    1.0 - HYPER_PARAMS["CLIPPING_COEFFICIENT"],
+                    1.0 + HYPER_PARAMS["CLIPPING_COEFFICIENT"],
+                )
+                policy_loss = torch.max(policy_loss1, policy_loss2).mean()
+
+                # Calculate value loss
+                new_value = new_value.view(-1)
+                if HYPER_PARAMS["CLIP_VALUELOSS"]:
+                    unclipped_value_loss = (
+                        new_value - batch_returns[minibatch_ind]
+                    ) ** 2
+                    clipped_value = batch_values[minibatch_ind] + torch.clamp(
+                        new_value - batch_values[minibatch_ind],
+                        -HYPER_PARAMS["CLIPPING_COEFFICIENT"],
+                        HYPER_PARAMS["CLIPPING_COEFFICIENT"],
+                    )
+                    clipped_value_loss = (
+                        clipped_value - batch_returns[minibatch_ind]
+                    ) ** 2
+                    max_value_loss = torch.max(unclipped_value_loss, clipped_value_loss)
+                    # NOT SURE IF 0.5 IS CORRECT
+                    value_loss = 0.5 * max_value_loss.mean()
+                else:
+                    # NOT SURE IF 0.5 IS CORRECT
+                    value_loss = (
+                        0.5 * ((new_value - batch_returns[minibatch_ind]) ** 2).mean()
+                    )
+
+                # Calculate entropy loss
+                entropy_loss = entropy.mean()
+
+                # Calculate total loss
+                loss = (
+                    policy_loss
+                    - HYPER_PARAMS["ENTROPY_COEFFICIENT"] * entropy_loss
+                    + HYPER_PARAMS["VALUE_LOSS_COEFFICIENT"] * value_loss
+                )
+
+                optimizer.zero_grad()
+                loss.backward()
+                if HYPER_PARAMS["NORMALIZE_GRADIENTS"]:
+                    nn.utils.clip_grad_norm_(
+                        policy.parameters(), HYPER_PARAMS["MAX_GRADIENT_NORM"]
+                    )
+                optimizer.step()
+
+                # Early stopping
+                if (
+                    HYPER_PARAMS["TARGET_KL"] is not None
+                    and new_kl > HYPER_PARAMS["TARGET_KL"]
+                ):
+                    break
+
+        # Calculate explained variance
+        y_predict, y_true = batch_values.cpu().numpy(), batch_returns.cpu().numpy()
+        var_y = np.var(y_true)
+        explained_var = np.nan if var_y == 0 else 1 - np.var(y_true - y_predict) / var_y
+
+        # Log results
+        # Debug variables are:
+        # - policy_loss: the mean policy loss across the minibatches
+        # - value_loss: the mean value loss across the minibatches
+        # - entropy_loss: the mean entropy loss across the minibatches
+        # - clipped_frac: the fraction of the minibatches where the policy loss was clipped
+        # - new_kl: the KL divergence between the old and new policy
+        writer.add_scalar(
+            "charts/learning_rate", optimizer.param_groups[0]["lr"], global_step
+        )
+        print("SPS:", int(global_step / (time.time() - start_time)))
+        writer.add_scalar(
+            "charts/SPS", int(global_step / (time.time() - start_time)), global_step
+        )
+        writer.add_scalar("losses/value_loss", value_loss.item(), global_step)
+        writer.add_scalar("losses/policy_loss", policy_loss.item(), global_step)
+        writer.add_scalar("losses/entropy_loss", entropy_loss.item(), global_step)
+        writer.add_scalar("losses/new_KL", new_kl.item(), global_step)
+        writer.add_scalar("losses/old_KL", old_kl.item(), global_step)
+        writer.add_scalar(
+            "losses/clippedfractions", np.mean(clipped_fracs), global_step
+        )
+        writer.add_scalar("losses/explained_variance", explained_var, global_step)
+
+envs.close()
+writer.close()
